ae08324361fc6dc71311c70ca35a5c61490b7cb5
Sanjeeb241 <sanjeebsamanta241@gmail.com>
2021-12-29 19:04:26 +0530

    Initial Commit

---
+++ Simpy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simpy
+//
+//  Created by Sanjeeb Samanta on 29/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
